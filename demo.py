54b067aac1273f7076e9d0a81e272b2fc92de26d
Ashok <ashokreddy4331@gmail.com>
2023-02-28 17:38:21 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,48 @@
+[200~ n=int(input("enter"))
+        # m=int(input("enter a number"))
+        # sum=n+m
+        # if sum==10:
+        #    print("fizz")
+        # elif  sum==20:
+        #    print("buzz")
+        # else :
+        #    print("fizzbuzz")
+
+        # def num(n,m):
+        #    sum=(n + m)
+        #    if sum == 10:
+        #      print("fizz")
+        #    elif  sum==20:
+        #      print("buzz")
+        #    else :
+        #      print("fizzbuzz")
+        #
+        # num(5,5)
+
+        # n="wow"
+        # if n==n[::-1]:
+        #   print("polidrum")
+        # else:
+        #   print("not polindrum")
+
+
+
+        # num="ashok kumar"
+        # a = num.split()
+        # for i in a:
+        #    print(i[::-1],end=" ")
+
+        # a="aabbcc"
+        # b=[]
+        # for i in a:
+        #    if i not in b:
+        #       b.append(i)
+        # print(b)
+
+        # a="abcd"
+        # b="bcda"
+        # if(sorted)(a)==(sorted)(b):
+        #    print("angstrom")
+        # else:
+        #    print("not a angstrom")
+        # 
